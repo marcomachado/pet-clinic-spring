14a704410910830cf37bdd8a6a4fc25706bbba4c
Marco Machado <marco.santos.machado@gmail.com>
2022-05-26 21:54:56 -0300

    adding owners list and controller

---
+++ petclinic/ptweb/src/main/com/masm/petclinic/petclinic/controllers/OwnersController.java
@@ -0,0 +1,14 @@
+package main.com.masm.petclinic.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnersController {
+    @RequestMapping({"","/","index", "index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+
+}
