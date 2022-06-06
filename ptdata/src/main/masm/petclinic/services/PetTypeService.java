5004be522d36385f9951f1889780c097af7c0a53
Marco Machado <marco.santos.machado@gmail.com>
2022-06-06 18:34:41 -0300

    org2

--- ptdata/src/main/masm/petclinic/services/PetTypeService.java
+++
@@ -1,9 +0,0 @@
-package main.masm.petclinic.services;
-
-import main.masm.petclinic.model.PetType;
-
-/**
- * Created by jt on 7/29/18.
- */
-public interface PetTypeService extends CrudService<PetType, Long> {
-}
